7eb0973d4a7c87080d405cf326ccfdf3ae05428c
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-06-11 17:55:01 -0300

    fixed props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,21 @@ import {
   useRef,
   useState,
   useCallback,
+  ReactNode,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string,
+  icon?: ReactNode,
+  placeholder: string,
+}
+
+export const Input = ({ name, icon: Icon, placeholder }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -44,10 +51,8 @@ const Input = ({ name, icon: Icon, ...rest }) => {
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
-        {...rest}
+        placeholder={placeholder}
       />
     </Container>
   );
 };
-
-export default Input;
